97fe1d41bfe99674065bf6eab1a9e0d48325ab2f
Palak Bhawsar <69889600+palakbhawsar98@users.noreply.github.com>
2023-01-23 18:22:01 +0530

    SQL script updated

--- script.sql
+++ script.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS 'user';
-CREATE TABLE 'user' (
-  'ID' int(11) NOT NULL AUTO_INCREMENT,
-  'FirstName' varchar(255) DEFAULT NULL,
-  'LastName' varchar(255) DEFAULT NULL,
-  'Age' int(2) DEFAULT NULL,
-  'UserName' varchar(255) DEFAULT NULL,
-  'Password' varchar(255) DEFAULT NULL,
-  'Cpassword' varchar(255) DEFAULT NULL,
-  PRIMARY KEY ('ID')
+CREATE TABLE fitness.user (
+  ID int(11) NOT NULL AUTO_INCREMENT,
+  FirstName varchar(255) DEFAULT NULL,
+  LastName varchar(255) DEFAULT NULL,
+  Age int(2) DEFAULT NULL,
+  UserName varchar(255) DEFAULT NULL,
+  Password varchar(255) DEFAULT NULL,
+  Cpassword varchar(255) DEFAULT NULL,
+  PRIMARY KEY (ID)
 );
+
